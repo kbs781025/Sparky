5443eb8c042322399aa771b10e7be247fee1f3ad
hjko <kbs781025@naver.com>
2018-05-12 21:18:23 +0900

    renderer command structure for renderer

---
+++ Sparky Core/src/graphics/RenderCommand.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "../maths/maths.h"
+
+namespace sparky { namespace graphics {
+
+	class Mesh;
+	class Shader;
+
+	struct RendererUniform
+	{
+		std::string uniform;
+		unsigned char* value;
+	};
+
+	struct RenderCommand
+	{
+		const Mesh* mesh;
+		const Shader* shader;
+		std::vector<RendererUniform> uniforms;
+		glm::mat4 transform;
+	};
+
+}}
